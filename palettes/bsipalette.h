f9b309a7ae8438c7874be5c724c86c209f4241db
elijah <elijah>
2021-08-01 18:57:55 +0300

    constructor 10

--- palettes/bsipalette.h
+++ palettes/bsipalette.h
@@ -108,6 +108,12 @@ public:
                unsigned int clr4, float brd4, unsigned int clr5, float brd5, unsigned int clr6, float brd6, 
                unsigned int clr7, float brd7, unsigned int clr8, float brd8, unsigned int clr9, float brd9, unsigned int clr10)
     { int b2=0, b1; PCD_CAST(clr1, brd1); PCD_CAST(clr2, brd2); PCD_CAST(clr3, brd3); PCD_CAST(clr4, brd4); PCD_CAST(clr5, brd5); PCD_CAST(clr6, brd6); PCD_CAST(clr7, brd7); PCD_CAST(clr8, brd8); PCD_CAST(clr9, brd9); PCD_LAST(clr10); }
+  PaletteBORDS(unsigned int clr1, float brd1, unsigned int clr2, float brd2, unsigned int clr3, float brd3, 
+               unsigned int clr4, float brd4, unsigned int clr5, float brd5, unsigned int clr6, float brd6, 
+               unsigned int clr7, float brd7, unsigned int clr8, float brd8, unsigned int clr9, float brd9,
+               unsigned int clr10, float brd10, unsigned int clr11)
+    { int b2=0, b1; PCD_CAST(clr1, brd1); PCD_CAST(clr2, brd2); PCD_CAST(clr3, brd3); PCD_CAST(clr4, brd4); PCD_CAST(clr5, brd5); 
+      PCD_CAST(clr6, brd6); PCD_CAST(clr7, brd7); PCD_CAST(clr8, brd8); PCD_CAST(clr9, brd9); PCD_CAST(clr10, brd10); PCD_LAST(clr11); }
 };
 
 /* QT_VERSION >= x050000 usage:
